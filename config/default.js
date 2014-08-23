e42473b38cc5dc3f1c3eca72ccc9069b609906b6
Alex <al.gaspar@gmail.com>
2014-08-23 21:45:38 +0300

    Remove mongodb reference

--- config/default.js
+++ config/default.js
@@ -4,6 +4,5 @@ var appEnv = process.env.NODE_ENV || 'development';
 
 module.exports = {
     appEnv    : appEnv
-  , mongo     : require('./mongo')[appEnv]
   , facebook  : require('./facebook')[appEnv]
 };
